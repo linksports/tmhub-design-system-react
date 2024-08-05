068d6ef70de64f4f0c132fa2c25608ce1a51419a
Naohiko MORI <naohiko.mori@linksports.co.jp>
2024-08-05 13:11:58 +0900

    temp

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,60 @@
 import { Slot } from '@radix-ui/react-slot';
 import { AsChildProps } from '../../util/AsChildProps';
 import { ButtonProps } from './Button.types';
-import { buttonRecipe } from './Button.css';
+// import { buttonRecipe } from './Button.css';
+import { recipe } from '@vanilla-extract/recipes';
+import { vars } from '../../tokens/vars.css';
+
+const buttonRecipe = recipe({
+  base: {
+    padding: `${vars.spacing.md} ${vars.spacing.lg}`,
+    border: '2px solid transparent',
+    borderRadius: '24px',
+    textDecoration: 'none',
+    transition: 'opacity .2s ease-out',
+    ':disabled': {
+      cursor: 'not-allowed',
+      opacity: 0.38,
+    },
+    selectors: {
+      '&:hover:not(:disabled)': {
+        cursor: 'pointer',
+      },
+      '&:active:not(:disabled)': {
+        opacity: 0.2,
+      },
+    },
+  },
+  variants: {
+    size: {
+      xs: { width: '60px' },
+      sm: { width: '80px' },
+      md: { width: '180px' },
+      lg: { width: '280px' },
+    },
+    variant: {
+      filled: {
+        borderColor: vars.color.brand,
+        backgroundColor: vars.color.brand,
+        color: vars.color.brandInverse,
+      },
+      outlined: {
+        borderColor: vars.color.brand,
+        backgroundColor: 'inherit',
+        color: vars.color.brand,
+      },
+      text: {
+        borderColor: 'transparent',
+        backgroundColor: 'inherit',
+        color: vars.color.text.primary,
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+    variant: 'filled',
+  },
+});
 
 const Button: React.FC<AsChildProps<ButtonProps, "button">> = ({
   asChild = false,
