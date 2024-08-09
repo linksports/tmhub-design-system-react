f6c531bcad56717540ec612416f9d504b5137095
Naohiko MORI <naohiko.mori@linksports.co.jp>
2024-08-09 14:22:22 +0900

    temp

--- src/components/Button/Button.css.ts
+++ src/components/Button/Button.css.ts
@@ -3,53 +3,57 @@ import { vars } from '../../tokens/vars.css';
 
 const button = recipe({
   base: {
-    padding: `${vars.spacing.md} ${vars.spacing.lg}`,
     border: '2px solid transparent',
     borderRadius: '24px',
-    textDecoration: 'none',
-    transition: 'opacity .2s ease-out',
-    ':disabled': {
-      cursor: 'not-allowed',
-      opacity: 0.38,
-    },
-    selectors: {
-      '&:hover:not(:disabled)': {
-        cursor: 'pointer',
-      },
-      '&:active:not(:disabled)': {
-        opacity: 0.2,
-      },
-    },
   },
-  variants: {
-    size: {
-      xs: { width: '60px' },
-      sm: { width: '80px' },
-      md: { width: '180px' },
-      lg: { width: '280px' },
-    },
-    variant: {
-      filled: {
-        borderColor: vars.color.brand,
-        backgroundColor: vars.color.brand,
-        color: vars.color.brandInverse,
-      },
-      outlined: {
-        borderColor: vars.color.brand,
-        backgroundColor: 'inherit',
-        color: vars.color.brand,
-      },
-      text: {
-        borderColor: 'transparent',
-        backgroundColor: 'inherit',
-        color: vars.color.text.primary,
-      },
-    },
-  },
-  defaultVariants: {
-    size: 'md',
-    variant: 'filled',
-  },
+  // base: {
+  //   padding: `${vars.spacing.md} ${vars.spacing.lg}`,
+  //   border: '2px solid transparent',
+  //   borderRadius: '24px',
+  //   textDecoration: 'none',
+  //   transition: 'opacity .2s ease-out',
+  //   ':disabled': {
+  //     cursor: 'not-allowed',
+  //     opacity: 0.38,
+  //   },
+  //   selectors: {
+  //     '&:hover:not(:disabled)': {
+  //       cursor: 'pointer',
+  //     },
+  //     '&:active:not(:disabled)': {
+  //       opacity: 0.2,
+  //     },
+  //   },
+  // },
+  // variants: {
+  //   size: {
+  //     xs: { width: '60px' },
+  //     sm: { width: '80px' },
+  //     md: { width: '180px' },
+  //     lg: { width: '280px' },
+  //   },
+  //   variant: {
+  //     filled: {
+  //       borderColor: vars.color.brand,
+  //       backgroundColor: vars.color.brand,
+  //       color: vars.color.brandInverse,
+  //     },
+  //     outlined: {
+  //       borderColor: vars.color.brand,
+  //       backgroundColor: 'inherit',
+  //       color: vars.color.brand,
+  //     },
+  //     text: {
+  //       borderColor: 'transparent',
+  //       backgroundColor: 'inherit',
+  //       color: vars.color.text.primary,
+  //     },
+  //   },
+  // },
+  // defaultVariants: {
+  //   size: 'md',
+  //   variant: 'filled',
+  // },
 });
 
 export default button;
