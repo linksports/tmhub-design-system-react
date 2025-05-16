6469e6459050c5dbc69a5344b93fa05fe30b1c20
Taro Tsuruga <taro.tsuruga@linksports.co.jp>
2025-05-16 15:10:49 +0900

    wip

---
+++ src/components/Header/Drawer.types.ts
@@ -0,0 +1,19 @@
+import { Iconography } from "../Icons";
+import { LabelProps } from "../Label/Label.types";
+
+export type MenuItemProps = {
+  label: string;
+  icon: keyof typeof Iconography;
+  badge?: LabelProps;
+  selected?: boolean;
+  as?: React.ElementType;
+} & React.ComponentProps<"a">;
+
+export type SectionProps = {
+  title?: string;
+  items: MenuItemProps[];
+};
+
+export type DrawerContentProps = {
+  sections: SectionProps[];
+} & React.ComponentProps<"div">;
